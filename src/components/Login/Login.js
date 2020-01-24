071c263a972bbdc18c6c079d79d03656d7b76b67
Mahkii900 <col.graff.battleschool@gmail.com>
2020-01-24 12:09:24 -0700

    added a store and login page of sorts

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -2,11 +2,62 @@ import React, {Component} from 'react'
 import {withRouter} from 'react-router-dom'
 
 export class Login extends Component {
+    state = {
+        username: '',
+        password: ''
+    }
+
+    usernameHandler(event) {
+        this.setState({username: event.target.value})
+    }
+
+    passwordHandler(event) {
+        this.setState({password: event.target.value})
+    }
+
+    signUp() {
+        alert("You signed up!")
+        this.props.history.push('/home')
+    }
+
     render() {
         return(
             <div>
-                Login
-                <button onClick={() => this.props.history.push('/home')}>Login</button>
+                <div>
+                    <h1>Hyperion Genesis</h1>
+                </div>
+                <div>
+                    <h2>Login</h2>
+                    <div>
+                        <div>
+                            <h3>Username</h3>
+                            <input type='text' value={this.state.username} onChange={(event) => this.usernameHandler(event)} />
+                        </div>
+                        <div>
+                            <h3>Password</h3>
+                            <input type='password' value={this.state.password} onChange={(event) => this.passwordHandler(event)} />
+                        </div>
+                    </div>
+                    <div>
+                        <button onClick={() => this.props.history.push('/home')}>Login</button>
+                    </div>
+                </div>
+                <div>
+                    <h2>Sign Up</h2>
+                    <div>
+                        <div>
+                            <h3>Username</h3>
+                            <input type='text' value={this.state.username} onChange={(event) => this.usernameHandler(event)} />
+                        </div>
+                        <div>
+                            <h3>Password</h3>
+                            <input type='text' value={this.state.password} onChange={(event) => this.passwordHandler(event)} />
+                        </div>
+                        <div>
+                            <button onClick={() => this.signUp()}>Sign Up</button>
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
---
+++ src/ducks/reducer.js
@@ -0,0 +1,17 @@
+//INITIAL STATE
+const initialState = {
+
+}
+
+//ACTION CONSTS
+
+
+//ACTION BUILDERS
+
+
+//REDUCER
+export default (state = initialState, action) => {
+    switch(action.type) {
+        default: return state
+    }
+}
